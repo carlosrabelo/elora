4f010dc00ce629a1902bc7d429e4eb6f2a832d64
Carlos Rabelo <developer@carlosrabelo.com.br>
2016-02-05 21:26:00 -0300

    feat: add Engine back-buffer drawing
    Rasterize into a CPU buffer and only blit on present, so tests can assert pixels without X11.

--- elora/src/engine/engine.cpp
+++ elora/src/engine/engine.cpp
@@ -94,30 +94,94 @@ bool Engine::run(std::function<void()> on_frame) {
     return true;
 }
 
-void Engine::clear(std::uint32_t /*color*/) {
-    // scaffold: drawing lands in a later commit
+void Engine::clear(std::uint32_t color) {
+    std::fill(back_buffer_.begin(), back_buffer_.end(), color);
 }
 
-void Engine::put_pixel(int /*x*/, int /*y*/, std::uint32_t /*color*/) {
-    // scaffold: drawing lands in a later commit
+void Engine::put_pixel(int x, int y, std::uint32_t color) {
+    const int bw = buffer_width();
+    const int bh = buffer_height();
+    if (x < 0 || y < 0 || x >= bw || y >= bh) {
+        return;
+    }
+    back_buffer_[static_cast<std::size_t>(y) * static_cast<std::size_t>(bw) + static_cast<std::size_t>(x)] = color;
 }
 
-void Engine::draw_line(int /*x0*/, int /*y0*/, int /*x1*/, int /*y1*/, std::uint32_t /*color*/) {
-    // scaffold: drawing lands in a later commit
+void Engine::draw_line(int x0, int y0, int x1, int y1, std::uint32_t color) {
+    const int dx = std::abs(x1 - x0);
+    const int sx = x0 < x1 ? 1 : -1;
+    const int dy = -std::abs(y1 - y0);
+    const int sy = y0 < y1 ? 1 : -1;
+    int err = dx + dy;
+    for (;;) {
+        put_pixel(x0, y0, color);
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
+        const int e2 = 2 * err;
+        if (e2 >= dy) {
+            err += dy;
+            x0 += sx;
+        }
+        if (e2 <= dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
 }
 
-void Engine::stroke_triangle(int /*x0*/, int /*y0*/, int /*x1*/, int /*y1*/, int /*x2*/, int /*y2*/,
-                             std::uint32_t /*color*/) {
-    // scaffold: drawing lands in a later commit
+void Engine::stroke_triangle(int x0, int y0, int x1, int y1, int x2, int y2, std::uint32_t color) {
+    draw_line(x0, y0, x1, y1, color);
+    draw_line(x1, y1, x2, y2, color);
+    draw_line(x2, y2, x0, y0, color);
 }
 
-void Engine::fill_triangle(int /*x0*/, int /*y0*/, int /*x1*/, int /*y1*/, int /*x2*/, int /*y2*/,
-                           std::uint32_t /*color*/) {
-    // scaffold: drawing lands in a later commit
+namespace {
+
+int edge(int ax, int ay, int bx, int by, int px, int py) {
+    return (bx - ax) * (py - ay) - (by - ay) * (px - ax);
 }
 
-std::uint32_t Engine::pixel(int /*x*/, int /*y*/) const {
-    return 0;
+}  // namespace
+
+void Engine::fill_triangle(int x0, int y0, int x1, int y1, int x2, int y2, std::uint32_t color) {
+    const int bw = buffer_width();
+    const int bh = buffer_height();
+    if (bw <= 0 || bh <= 0 || back_buffer_.empty()) {
+        return;
+    }
+
+    const int area = edge(x0, y0, x1, y1, x2, y2);
+    if (area == 0) {
+        return;
+    }
+
+    const int min_x = std::max(0, std::min(x0, std::min(x1, x2)));
+    const int min_y = std::max(0, std::min(y0, std::min(y1, y2)));
+    const int max_x = std::min(bw - 1, std::max(x0, std::max(x1, x2)));
+    const int max_y = std::min(bh - 1, std::max(y0, std::max(y1, y2)));
+
+    for (int y = min_y; y <= max_y; ++y) {
+        for (int x = min_x; x <= max_x; ++x) {
+            const int w0 = edge(x1, y1, x2, y2, x, y);
+            const int w1 = edge(x2, y2, x0, y0, x, y);
+            const int w2 = edge(x0, y0, x1, y1, x, y);
+            const bool inside = (area > 0) ? (w0 >= 0 && w1 >= 0 && w2 >= 0)
+                                           : (w0 <= 0 && w1 <= 0 && w2 <= 0);
+            if (inside) {
+                put_pixel(x, y, color);
+            }
+        }
+    }
+}
+
+std::uint32_t Engine::pixel(int x, int y) const {
+    const int bw = buffer_width();
+    const int bh = buffer_height();
+    if (x < 0 || y < 0 || x >= bw || y >= bh) {
+        return 0;
+    }
+    return back_buffer_[static_cast<std::size_t>(y) * static_cast<std::size_t>(bw) + static_cast<std::size_t>(x)];
 }
 
 void Engine::present() {
--- elora/tests/test_engine.cpp
+++ elora/tests/test_engine.cpp
@@ -49,6 +49,80 @@ TEST_CASE("Engine pixel width and height are independent") {
     REQUIRE(engine.buffer_height() == 120);
 }
 
+TEST_CASE("Engine put_pixel writes to the framebuffer") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.put_pixel(10, 20, 0x00ff00);
+    REQUIRE(engine.pixel(10, 20) == 0x00ff00);
+}
+
+TEST_CASE("Engine present without a window leaves the back buffer") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.put_pixel(4, 5, 0xabcdef);
+    engine.present();
+    REQUIRE(engine.pixel(4, 5) == 0xabcdef);
+}
+
+TEST_CASE("Engine put_pixel ignores out of bounds") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.put_pixel(-1, 0, 0xffffff);
+    engine.put_pixel(0, -1, 0xffffff);
+    engine.put_pixel(160, 0, 0xffffff);
+    engine.put_pixel(0, 120, 0xffffff);
+    REQUIRE(engine.pixel(0, 0) == 0);
+}
+
+TEST_CASE("Engine clear fills the framebuffer") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.clear(0x112233);
+    REQUIRE(engine.pixel(0, 0) == 0x112233);
+    REQUIRE(engine.pixel(159, 119) == 0x112233);
+}
+
+TEST_CASE("Engine fill_triangle covers interior pixels") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.fill_triangle(0, 0, 20, 0, 0, 20, 0xff0000);
+    REQUIRE(engine.pixel(2, 2) == 0xff0000);
+    REQUIRE(engine.pixel(1, 1) == 0xff0000);
+}
+
+TEST_CASE("Engine fill_triangle leaves outside pixels") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.fill_triangle(0, 0, 20, 0, 0, 20, 0xff0000);
+    REQUIRE(engine.pixel(19, 19) == 0);
+}
+
+TEST_CASE("Engine fill_triangle skips degenerate") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.fill_triangle(5, 5, 5, 5, 10, 5, 0xffffff);
+    REQUIRE(engine.pixel(5, 5) == 0);
+}
+
+TEST_CASE("Engine draw_line marks endpoints") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.draw_line(2, 3, 8, 3, 0x00ff00);
+    REQUIRE(engine.pixel(2, 3) == 0x00ff00);
+    REQUIRE(engine.pixel(5, 3) == 0x00ff00);
+    REQUIRE(engine.pixel(8, 3) == 0x00ff00);
+}
+
+TEST_CASE("Engine stroke_triangle draws edges") {
+    elora::Engine engine{"test", 640, 480, 4, 4};
+    engine.init();
+    engine.stroke_triangle(0, 0, 10, 0, 0, 10, 0xffffff);
+    REQUIRE(engine.pixel(0, 0) == 0xffffff);
+    REQUIRE(engine.pixel(10, 0) == 0xffffff);
+    REQUIRE(engine.pixel(0, 10) == 0xffffff);
+    REQUIRE(engine.pixel(5, 0) == 0xffffff);
+}
+
 TEST_CASE("Engine keys start up") {
     elora::Engine engine{"test", 640, 480, 4, 4};
     REQUIRE_FALSE(engine.is_key_down(elora::Key::W));
