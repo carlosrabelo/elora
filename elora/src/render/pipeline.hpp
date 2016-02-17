1f8e77e7f35e753f28cb0bd26361546a3ee4eeae
Carlos Rabelo <developer@carlosrabelo.com.br>
2016-02-16 21:51:00 -0300

    feat: add triangle projection pipeline
    Rotate, project, and cull faces before any pixel is written.

---
+++ elora/src/render/pipeline.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <core/optional.hpp>
+#include <math/camera.hpp>
+#include <math/vector3d.hpp>
+#include <mesh/mesh.hpp>
+#include <mesh/triangle.hpp>
+
+#include <algorithm>
+
+namespace elora {
+
+struct ScreenTriangle {
+    ScreenPoint v0;
+    ScreenPoint v1;
+    ScreenPoint v2;
+};
+
+inline int screen_area(int x0, int y0, int x1, int y1, int x2, int y2) {
+    return (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0);
+}
+
+inline bool is_off_screen(const ScreenTriangle& triangle, int buffer_width, int buffer_height) {
+    const int min_x = std::min(triangle.v0.x, std::min(triangle.v1.x, triangle.v2.x));
+    const int max_x = std::max(triangle.v0.x, std::max(triangle.v1.x, triangle.v2.x));
+    const int min_y = std::min(triangle.v0.y, std::min(triangle.v1.y, triangle.v2.y));
+    const int max_y = std::max(triangle.v0.y, std::max(triangle.v1.y, triangle.v2.y));
+    return max_x < 0 || min_x >= buffer_width || max_y < 0 || min_y >= buffer_height;
+}
+
+inline Optional<ScreenTriangle> project_triangle(const Vector3D& p0, const Vector3D& p1,
+                                                 const Vector3D& p2, const Camera& camera,
+                                                 int buffer_width, int buffer_height) {
+    const auto s0 = camera.project(p0, buffer_width, buffer_height);
+    const auto s1 = camera.project(p1, buffer_width, buffer_height);
+    const auto s2 = camera.project(p2, buffer_width, buffer_height);
+    if (!s0 || !s1 || !s2) {
+        return {};
+    }
+    if (screen_area(s0->x, s0->y, s1->x, s1->y, s2->x, s2->y) > 0) {
+        return {};
+    }
+    const ScreenTriangle screen{*s0, *s1, *s2};
+    if (is_off_screen(screen, buffer_width, buffer_height)) {
+        return {};
+    }
+    return screen;
+}
+
+inline Optional<ScreenTriangle> transform_and_project(const Mesh& mesh, const Triangle& triangle,
+                                                           float angle_x, float angle_y, float angle_z,
+                                                           const Camera& camera, int buffer_width,
+                                                           int buffer_height) {
+    const auto p = mesh.positions(triangle);
+    return project_triangle(p[0].rotated(angle_x, angle_y, angle_z), p[1].rotated(angle_x, angle_y, angle_z),
+                            p[2].rotated(angle_x, angle_y, angle_z), camera, buffer_width, buffer_height);
+}
+
+}  // namespace elora
---
+++ elora/tests/test_pipeline.cpp
@@ -0,0 +1,39 @@
+#include <catch.hpp>
+#include <math/camera.hpp>
+#include <mesh/mesh.hpp>
+#include <mesh/primitives.hpp>
+#include <render/pipeline.hpp>
+
+TEST_CASE("screen_area is positive for clockwise screen winding") {
+    REQUIRE(elora::screen_area(0, 0, 10, 0, 0, 10) == 100);
+}
+
+TEST_CASE("screen_area is negative for counter-clockwise screen winding") {
+    REQUIRE(elora::screen_area(0, 0, 0, 10, 10, 0) == -100);
+}
+
+TEST_CASE("project_triangle keeps a front-facing cube face") {
+    const elora::Mesh cube = elora::make_cube();
+    const elora::Camera camera{{0, 0, 3}, {0, 0, 0}, {0, 1, 0}, 60.0f};
+    const auto screen = elora::transform_and_project(cube, cube.triangles[0], 0, 0, 0, camera, 160, 120);
+    REQUIRE(screen.has_value());
+}
+
+TEST_CASE("project_triangle culls a back-facing cube face") {
+    const elora::Mesh cube = elora::make_cube();
+    const elora::Camera camera{{0, 0, -3}, {0, 0, 0}, {0, 1, 0}, 60.0f};
+    const auto screen = elora::transform_and_project(cube, cube.triangles[0], 0, 0, 0, camera, 160, 120);
+    REQUIRE_FALSE(screen.has_value());
+}
+
+TEST_CASE("project_triangle rejects a triangle behind the camera") {
+    const elora::Camera camera{{0, 0, 3}, {0, 0, 0}, {0, 1, 0}, 60.0f};
+    const auto screen = elora::project_triangle({-1, -1, 5}, {1, -1, 5}, {0, 1, 5}, camera, 160, 120);
+    REQUIRE_FALSE(screen.has_value());
+}
+
+TEST_CASE("project_triangle rejects a triangle that misses the buffer") {
+    const elora::Camera camera{{0, 0, 3}, {0, 0, 0}, {0, 1, 0}, 60.0f};
+    const auto screen = elora::project_triangle({-20, -20, 0}, {-18, -20, 0}, {-19, -18, 0}, camera, 160, 120);
+    REQUIRE_FALSE(screen.has_value());
+}
